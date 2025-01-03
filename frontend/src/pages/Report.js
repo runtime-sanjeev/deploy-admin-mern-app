67217cf2c116e65bd61c8fccc58ba7dcaae490f2
runtime-sanjeev <runtime.sanjeev@gmail.com>
2025-01-03 17:39:12 +0530

    report

---
+++ frontend/src/pages/Report.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Report() {
+  return (
+    <div>
+        <h3>Download Report</h3>
+        <p>Comming Soon</p>
+        
+    </div>
+  )
+}
+
+export default Report
