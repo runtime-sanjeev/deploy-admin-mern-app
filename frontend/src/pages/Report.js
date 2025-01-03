b2277dbe3506172586d2eec276c410e3420db38f
runtime-sanjeev <runtime.sanjeev@gmail.com>
2025-01-03 18:15:24 +0530

    reports

--- frontend/src/pages/Report.js
+++ frontend/src/pages/Report.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Header from '../component/header'
+import Sidebar from '../component/sidebar'
 
 function Report() {
   return (
     <div>
-        <h3>Download Report</h3>
+    <Header/>
+      <div className='content-container'>
+      <Sidebar/>
+      <aside className="main_content">
+        <div className='reg_container'>
+        <h1>Employee Reports</h1>
         <p>Comming Soon</p>
-        
+        </div>
+       </aside>
+    </div>
     </div>
   )
 }
