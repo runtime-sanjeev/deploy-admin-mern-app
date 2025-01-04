e03222b436247007e2c1cfda6c312d5c349b13da
runtime-sanjeev <runtime.sanjeev@gmail.com>
2025-01-04 21:26:19 +0530

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,17 @@ import Registration from "./pages/Registration";
 import Employee from "./pages/Employee";
 import Report from "./pages/Report";
 import Dashboard from "./pages/Dashboard";
-import { useState } from "react";
-import RefreshHandler from "./RefreshHandler";
+// import { useState } from "react";
+// import RefreshHandler from "./RefreshHandler";
 
 function App() {
-  const[isAuthenticated, setIsAuthenticated] = useState(false);
-  const PrivateRoute = ({element}) => {
-    return isAuthenticated ? element : <Navigate to={'/login'} />
-  }
+  // const[isAuthenticated, setIsAuthenticated] = useState(false);
+  // const PrivateRoute = ({element}) => {
+  //   return isAuthenticated ? element : <Navigate to={'/login'} />
+  // }
   return (
     <div className="app">
-      <RefreshHandler setIsAuthenticated={setIsAuthenticated}></RefreshHandler>
+      {/* <RefreshHandler setIsAuthenticated={setIsAuthenticated}></RefreshHandler> */}
       <Routes>
         <Route path="/" element = {<Navigate to ='/login'/>}></Route>
        <Route path="/login" element={<Login />}></Route>
@@ -23,7 +23,7 @@ function App() {
        <Route path="/signup" element={<Signup />}></Route>
        <Route path="/registration" element={<Registration />}></Route>
        <Route path="/employee" element={<Employee />}></Route>
-       <Route path="/dashboard" element ={<PrivateRoute element={<Dashboard />} />}></Route>
+       <Route path="/dashboard" element={<Dashboard />}></Route>
 
       </Routes>
     </div>
