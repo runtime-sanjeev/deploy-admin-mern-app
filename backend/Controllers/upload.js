75c2ae6cf0af3ef1bccf1ca7812a69588cfc7396
runtime-sanjeev <runtime.sanjeev@gmail.com>
2025-01-05 12:40:29 +0530

    cvc

--- backend/Controllers/upload.js
+++ backend/Controllers/upload.js
@@ -9,9 +9,9 @@ const storage = multer.diskStorage(
   destination: (req, file, cb) => {
     // console.log(file);
     if(file.fieldname == 'file'){
-      cb(null, 'https://github.com/runtime-sanjeev/deploy-admin-mern-app/tree/18b75708e3faa8a684b56be4f26d9506821a05be/backend/public/document/');  // Path where files are stored
+      cb(null, 'public/document/');  // Path where files are stored
     }else{
-      cb(null, 'https://github.com/runtime-sanjeev/deploy-admin-mern-app/tree/18b75708e3faa8a684b56be4f26d9506821a05be/backend/public/photo/');  // Path where files are stored
+      cb(null, 'public/photo/');  // Path where files are stored
     }
     
   },
