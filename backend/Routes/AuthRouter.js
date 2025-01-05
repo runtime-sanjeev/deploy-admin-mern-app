750256859d1367f27286e18e21a3bc0a14d9548d
runtime-sanjeev <runtime.sanjeev@gmail.com>
2025-01-05 12:01:53 +0530

    fgh

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,6 +1,6 @@
 const { signup, login, registration, employee, editemployee, updateemployee } = require('../Controllers/AuthController');
 const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
-const uploadFiles = require('../Controllers/upload'); // Import the upload middleware
+const upload = require('../Controllers/upload'); // Import the upload middleware
 const router = require('express').Router();
 
 // Signup route: Validate input and call the signup controller
@@ -10,7 +10,7 @@ router.post('/signup', signupValidation, signup);
 router.post('/login', loginValidation, login);
 
 // Registration route: Handle employee registration with file upload
-router.post('/registration', uploadFiles, registration);
+router.post('/registration', upload, registration);
 
 // Employee route: Get employee data (use GET instead of POST)
 router.get('/employee', employee);  // Use GET instead of POST
