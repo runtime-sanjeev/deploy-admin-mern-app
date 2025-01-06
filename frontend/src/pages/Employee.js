916423367768e732d51f74e6bfd8fac85e5c51f3
runtime-sanjeev <runtime.sanjeev@gmail.com>
2025-01-06 14:15:37 +0530

    zz

--- frontend/src/pages/Employee.js
+++ frontend/src/pages/Employee.js
@@ -139,21 +139,29 @@ function Employee() {
     {
       name: 'Uploaded Document',
       cell: (row) => (
-        <button onClick={() => viewDoc(row)} className="btn-view-doc">View Document</button>
+        <button onClick={() => viewDoc(row)} className="focus:outline-none text-white bg-green-700
+        hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-[0.675rem] px-5
+         py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">View Document</button>
       ),
       sortable: false,
     },
     {
       name: 'Uploaded Image',
       cell: (row) => (
-        <button onClick={() => viewImage(row)} className="btn-view-image">View Image</button>
+        <button onClick={() => viewImage(row)} className="focus:outline-none text-white
+        bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium 
+        rounded-lg text-[0.675rem] px-5 py-2.5 me-2 mb-2 dark:bg-green-600 
+        dark:hover:bg-green-700 dark:focus:ring-green-800">View Image</button>
       ),
       sortable: false,
     },
     {
       name: 'Action',
       cell: (row) => (
-        <button onClick={(e) => handleEdit(row, e)} className="btn-edit">Edit</button>
+        <button onClick={(e) => handleEdit(row, e)} className="btfocus:outline-none text-white bg-purple-700
+        hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg 
+        text-[0.675rem] px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 
+        dark:focus:ring-purple-800nedt">Edit</button>
       ),
       sortable: false,
     },
